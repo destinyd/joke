e30239c49c4bf62a2617169b07c765f6d2bf716b
destinyd <destinyd_war@163.com>
2013-11-04 20:54:42 +0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ddjoke::Application.config.secret_token = '28da07ba7fd210ca359288ea333b101a50d91b2e613d7683e4c2f485e20a871b361a095076ef8afb610e42ba69420758d8b0ae91750e132b07b27789dde0090c'
