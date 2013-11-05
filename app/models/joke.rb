5aa5cfa3e3666ad0f75d9255a7a2ce4a72028087
destinyd <destinyd_war@163.com>
2013-11-06 02:05:49 +0800

    show next prev

--- app/models/joke.rb
+++ app/models/joke.rb
@@ -13,9 +13,21 @@ class Joke
   field :videourl, type: String
   validates :joke_id, presence: true, uniqueness: true
   scope :recent, desc(:created_at)
+  scope :older, asc(:created_at)
   scope :short, where(:tags.in => ['短篇'])
   scope :long, where(:tags.in => ['长篇'])
   scope :image, where(:tags.in => ['有图'])
 
+  scope :newer_by, lambda{|joke| where(:created_at.gt => joke.created_at).older}
+  scope :older_by, lambda{|joke| where(:created_at.lt => joke.created_at).recent}
+
   taggable_on :tags
+
+  def older
+    @older ||= Joke.older_by(self).first
+  end
+
+  def newer
+    @newer ||= Joke.newer_by(self).first
+  end
 end
