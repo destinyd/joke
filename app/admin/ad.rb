af8575ce5df17bfb60bf7e2bb77377e50a450cbc
destinyd <destinyd_war@163.com>
2013-11-06 16:48:44 +0800

    content ads

--- app/admin/ad.rb
+++ app/admin/ad.rb
@@ -5,13 +5,14 @@ ActiveAdmin.register Ad do
       link_to Ad.ids[ad.ad_type], wojiudiaochuo_ad_path(ad)
     end
     column :finished_at
+    actions
   end
 
 
   form do |f|
     f.inputs do
-      f.input :content
       f.input :ad_type, collection: Ad.types, include_blank: false
+      f.input :content, as: :text
     end
     f.actions
   end
