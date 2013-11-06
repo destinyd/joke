f0d4b497a16c796301ab1c7be6e71bfb565de053
destinyd <destinyd_war@163.com>
2013-11-06 10:37:44 +0800

    comments recent

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,13 @@
 //= require jquery_ujs
 //= require bootstrap
 //= require_tree .
+var duoshuoQuery = {short_name:"ddjoke"};
 $(function() {
   $("img.lazy").lazyload();
+  var ds = document.createElement('script');
+  ds.type = 'text/javascript';ds.async = true;
+  ds.src = 'http://static.duoshuo.com/embed.js';
+  ds.charset = 'UTF-8';
+  (document.getElementsByTagName('head')[0] 
+   || document.getElementsByTagName('body')[0]).appendChild(ds);
 })
