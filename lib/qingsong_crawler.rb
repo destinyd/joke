d7cd119b9c3342f27e7603d375efce543632e4bf
destinyd <destinyd_war@163.com>
2013-11-07 13:24:28 +0800

    good qingsong crawler

--- lib/qingsong_crawler.rb
+++ lib/qingsong_crawler.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 require 'open-uri'
 
 class QingsongCrawler
@@ -13,11 +14,7 @@ class QingsongCrawler
   end
 
   def get_cotent(url)
-    begin
       open(url).read.gsub('http://17qs.qiniudn.com/img/grey.gif','/assets/loading_bar.gif').match(regex_wrap)['content']
-    rescue Exception => ex
-      p 'QingsongCrawler get error:' + url
-    end
   end
 
   def get_rss
@@ -27,15 +24,39 @@ class QingsongCrawler
   def get_recent
     get_rss.xpath('//item').each do |item|
       joke_id =  item.xpath('guid').text
-      unless Joke.where(joke_id: joke_id).first
-        name = item.xpath('title').text
-        link = item.xpath('link').text
-        Joke.create joke_id: joke_id, name: name, text: get_cotent(link), tag_list: '长篇,轻松'
+      name = item.xpath('title').text
+      link = item.xpath('link').text
+      begin
+        if is_need?(name, link)
+          if regex_short_url.match(link)
+            Joke.create joke_id: joke_id, name: name, text: get_cotent(link), tag_list: '短篇,轻松'
+          else
+            Joke.create joke_id: joke_id, name: name, text: get_cotent(link), tag_list: '长篇,轻松'
+          end unless Joke.where(joke_id: joke_id).first
+        end
+      rescue Exception => ex
+        p 'QingsongCrawler get error:' + link
       end
     end
   end
 
+  def is_need?(name, link)
+    return true if regex_need_url.match(link) or regex_qingsong_name.match(name)
+  end
+
   def regex_wrap
     @regex_wrap ||= /<div class="news-content.*?">(?<content>.*?)<\/div><br><script>var cpro_id/m
+  end
+
+  def regex_need_url
+    @regex_need_url ||= /news-(?:\d+$|tucao.*|FUN\d+)/
+  end
+
+  def regex_qingsong_name
+    @regex_qingsong_name ||= /轻松一刻/
+  end
+
+  def regex_short_url
+    @regex_short_url ||= /news-\d+$/
   end
 end
