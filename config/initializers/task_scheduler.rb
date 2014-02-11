e799b26682bd2daa144063cb733d97a278f91763
destinyd <destinyd_war@163.com>
2014-02-12 00:37:00 +0800

    refresh sitemap every day

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -5,3 +5,15 @@ scheduler.every "1h", :first_at => Time.now + 60 do
   JokeCrawler.new({all:false}).run
   QingsongCrawler.new.run
 end
+
+scheduler.every "1d", first_at: '1h' do
+  verbose = if SitemapGenerator::Utilities.truthy?(ENV['VERBOSE'])
+              true
+            elsif SitemapGenerator::Utilities.falsy?(ENV['VERBOSE'])
+              false
+            else
+              nil
+            end
+  SitemapGenerator::Interpreter.run(:config_file => ENV["CONFIG_FILE"], :verbose => verbose)
+  SitemapGenerator::Sitemap.ping_search_engines if Rails.env.production?
+end
