d9d4f39e7d37194536b0b089c2e8fab610103e3f
destinyd <destinyd_war@163.com>
2014-03-09 03:54:20 -0400

    merge mp

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Ddjoke::Application.routes.draw do
+  match 'yixin' => 'home#yixin'
+  get 'download' => "home#download", as: :download
+  get 'jokes(-p:page)' => 'jokes#index', as: :jokes, defaults: {page: 1}
+  get 'short(-p:page)' => 'jokes#short', as: :short, defaults: {page: 1}
+  get 'long(-p:page)' => 'jokes#long', as: :long, defaults: {page: 1}
+  get 'image(-p:page)' => 'jokes#image', as: :image, defaults: {page: 1}
+  get 'video(-p:page)' => 'jokes#video', as: :video, defaults: {page: 1}
+  resources :jokes
   root :to => "home#index"
 end
