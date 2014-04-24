0110b7f64e0cb6b6bca019caeea6485d1f7c599a
destinyd <destinyd_war@163.com>
2014-04-25 02:43:01 +0800

    fix some bug

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,4 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 Ddjoke::Application.config.secret_token = '28da07ba7fd210ca359288ea333b101a50d91b2e613d7683e4c2f485e20a871b361a095076ef8afb610e42ba69420758d8b0ae91750e132b07b27789dde0090c'
-Ddjoke::Application.config.secret_key_base = 'xx762a2f23950e306261908d4e5519ffe71ce626b119e9fc03a012ba86f46d82ef32d72f283633bacc2f59cf94ce5968552fe97d157e7f00560c1217d0029dda09'
+Ddjoke::Application.config.secret_key_base = 'f908ebee93069323dde84302ce178a64a75de160dc531e28a23de6d1eef4302e93f5e81d90de5013281be441bb22559d493b4a31de650c154d2d154be97f2b7f'
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_ddjoke_session'
+Rails.application.config.session_store :cookie_store, key: '_ddjoke1_session'
