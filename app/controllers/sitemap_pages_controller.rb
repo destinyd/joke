841102d3f230d756c1c48fd5d01bbbdbd3f8f070
destinyd <destinyd_war@163.com>
2014-07-21 00:40:04 +0000

    fix sitemap page cache bug

--- app/controllers/sitemap_pages_controller.rb
+++ app/controllers/sitemap_pages_controller.rb
@@ -1,5 +1,5 @@
 class SitemapPagesController < ApplicationController
-  caches_action :index, expires_in: 1.day, cache_url: Proc.new {|c| c.request.url }
+  #caches_action :index, expires_in: 1.day, cache_url: Proc.new {|c| c.request.url }
   def index
     @jokes = Joke.page(params[:page]).per(1000)
     add_crumb(I18n.t("action.sitemap_pages.index"), sitemap_pages_url)
