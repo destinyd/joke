84db8b4503bcc3ca0d3bbe24934ad13e7f0ed644
destinyd <destinyd_war@163.com>
2014-08-07 11:21:56 +0000

    task

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -1,10 +1,16 @@
 require 'rufus/scheduler'
 
 scheduler = Rufus::Scheduler.new
-scheduler.every "1h", :first_at => Time.now + 600 do
+scheduler.every "13m", :first_at => Time.now + 600 do
   JokeCrawler.new({all:false}).run
+end
+scheduler.every "59m", :first_at => Time.now + 660 do
   QingsongCrawler.new.run
+end
+scheduler.every "53m", :first_at => Time.now + 720 do
   QiushibaikeCrawler.new.run
+end
+scheduler.every "47m", :first_at => Time.now + 780 do
   BaozoumanhuaCrawler.new.run
 end
 
